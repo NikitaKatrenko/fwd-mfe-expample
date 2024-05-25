33ff0ff742e8897266d58e048df0924e9ebef8f1
akhudiak <next.teacher@lectrum.io>
2024-05-26 00:04:13 +0100

    watchlist integrated

--- mfe-wishlist/vite.config.ts
+++ mfe-wishlist/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "watchlist",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Watchlist": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
